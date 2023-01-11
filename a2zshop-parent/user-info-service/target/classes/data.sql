3c203506532d859e562772bc17a65e9266ddc2f2
Velan <velunachimuthu@gmail.com>
2023-01-11 17:52:53 +0530

    Removing target files

--- a2zshop-parent/user-info-service/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into address(id,door_no,street,city,state,pincode)
-            values(101,'536','abc street', 'kgm','TN','638701');
-insert into user_details(id,email,mobile_number,password,user_name,address_id)
-                values(1001,'abc@efg.hi','6384912345','password','velan',101);
